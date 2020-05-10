0d071cebf53c6f4ce26367ebe753a3d6c832d377
Chaston Harper <chastonharper1@gmail.com>
2020-05-10 18:44:13 -0500

    wrote sayHello function

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var firstName = 'Chaston';
 const states = 50;
 var val = 5 + 4;
+// code is functional when logged to the console //
+function sayHello() {
+    console.log('Hello World!');
+}
+sayHello();
+
