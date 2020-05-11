6928a192d1664aa3a6c758a37d3dc9a757749ece
Chaston Harper <chastonharper1@gmail.com>
2020-05-10 21:26:01 -0500

    Added checkage function and alert

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ function sayHello() {
 }
 sayHello();
 
+function checkAge(name, age) {
+    if (age < 21) {
+        alert("Sorry" + name + ", you aren't old enough to view this page!");
+    }
+}
+
+let charles = {
+    name: "Charles",
+    age: 21
+};
+let abby = {
+    name: "Abby",
+    age: 27
+};
+let james = {
+    name: "James",
+    age: 18
+};
+let john = {
+    name: "John",
+    age: 17
+};
+
